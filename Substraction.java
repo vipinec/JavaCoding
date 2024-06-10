27f86a343115cfb3befa13d403d40cc5360eb8d7
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-10 17:00:20 +0530

    added two files

---
+++ Substraction.java
@@ -0,0 +1,16 @@
+package JavaCoding;
+
+public class Substraction {
+    public static void main(String[] args){
+
+
+        int a= 20;
+        int b=10;
+
+
+        int sub = a-b;
+
+        System.out.println(sub);
+    }
+    
+}
