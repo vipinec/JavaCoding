480f2804e5449579f00ea81efe8447d1b5440bb9
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-10 16:34:12 +0530

    adding two file gun.java and inxed.html

---
+++ gun.java
@@ -0,0 +1,11 @@
+package JavaCoding;
+
+public class gun {
+        public static void main(String[] args){
+    
+    
+            System.out.println("hello first java program");
+        }
+    }
+    
+
