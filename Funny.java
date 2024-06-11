b33a35c7224015acd5bb474eba9d153b4c48dc94
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-11 21:43:07 +0530

    multiple added

---
+++ Funny.java
@@ -0,0 +1,14 @@
+package JavaCoding;
+
+public class Funny {
+
+    public static void main(String[] args){
+
+        int a=2;
+        int b=3;
+
+
+        System.out.println(a*b);
+    }
+    
+}
