5d36746c414961a6e353879a85ebf8ab498af41c
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-12 10:59:51 +0530

    multiple fuctions added

---
+++ MultiParametFunction.java
@@ -0,0 +1,16 @@
+package JavaCoding;
+
+public class MultiParametFunction {
+
+    public static String multiparameter(String name, String age){
+
+        return name+age;
+
+
+    }
+    public static void main(String[] args){
+      System.out.print(multiparameter("Colorname", " " +"twentry fine +"));
+
+    }
+    
+}
---
+++ MultipleFunctionnoreturn.java
@@ -0,0 +1,17 @@
+package JavaCoding;
+
+public class MultipleFunctionnoreturn {
+
+
+    public static void noretun(int age, int number){
+
+        System.out.println(age + " " + number);
+
+
+    }
+    public static void main(String[] args){
+
+        noretun(23, 2);
+    }
+    
+}
