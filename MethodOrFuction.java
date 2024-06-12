9120d2de9db06ed6be07469e813feef074d5b91a
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-12 10:17:42 +0530

    static fuction added

---
+++ MethodOrFuction.java
@@ -0,0 +1,23 @@
+package JavaCoding;
+
+public class MethodOrFuction {
+
+
+
+        public static void functionvk(){
+
+            System.out.println("my fuction static no return value");
+        }
+
+        public static void main(String[] args){
+
+            functionvk();
+            functionvk();
+            functionvk();
+
+
+            // three times fution calling
+
+        }
+    }
+    
