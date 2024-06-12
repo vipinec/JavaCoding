56dc9cccee3b91f7b7305e7e3eb34268cb129761
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-12 13:58:07 +0530

    hello java

---
+++ ClassB.java
@@ -0,0 +1,15 @@
+package JavaCoding;
+
+public class ClassB extends ClassA{
+    public static void main(String[] nargs){
+
+    ClassB obj = new ClassB();
+    
+    obj.main();
+    obj.main1();
+
+    System.out.println(obj.x + " " + obj.y + " " + obj.d + "" + obj.g);
+
+    
+}
+}
---
+++ Opps.java
@@ -0,0 +1,17 @@
+package JavaCoding;
+
+public class Opps {
+    int u=5;
+    public void caller(){
+    System.out.println("hello oops concept");
+
+    }
+    public static void main(String[] args){
+
+   Opps obj = new Opps();
+   obj.caller();
+   System.out.println(obj.u);
+
+    }
+    
+}
