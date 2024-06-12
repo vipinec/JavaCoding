be9196e9a71c6b2fec1dd45cd38fd1a03a6d12de
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-12 11:58:33 +0530

    hek

---
+++ ReturnNewValue.java
@@ -0,0 +1,19 @@
+package JavaCoding;
+
+public class ReturnNewValue {
+
+    static int howreturn(int x){
+return 5+x;
+
+    }
+
+    public static void main(String[] args){
+
+        int x=6;
+
+        System.out.println(howreturn(3));
+
+
+    }
+    
+}
