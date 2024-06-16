4f0ecf542cf366a8d9bb34bdc6c923e019a208a8
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-16 14:44:22 +0530

    s

---
+++ JavaCoding/Hi.java
@@ -0,0 +1,9 @@
+package JavaCoding;
+
+public class Hi {
+public static void main(String[] arhs){
+
+
+    System.out.println("hi dosto my action set done hai");
+}    
+}
---
+++ JavaCoding/JaiShree.java
@@ -0,0 +1,14 @@
+package JavaCoding;
+
+public class JaiShree {
+    public static void main(String[] arhs){
+
+        int a=1;
+        int b=2;
+
+
+System.out.println(a+b);
+
+    }
+    
+}
