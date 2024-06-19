1c07bee907d8f9cd4d878b7581e24f7c29938a58
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-19 16:27:14 +0530

    final

--- JavaCoding/DeleteFile.java
+++ JavaCoding/DeleteFile.java
@@ -4,8 +4,8 @@ public class DeleteFile {
     public static void main(String[] args){
 
 
-        System.out.println("deletefile java added the  reset file");
-        System.out.println("deletefile java added the  reset file new fime second time");
+        System.out.println("deletefile j");
+        
     }
     
 }
