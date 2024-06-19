c72afbe0cfe135fef5705e8806d55064b18f2ceb
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-19 15:19:45 +0530

    sum added

---
+++ JavaCoding/AddTwoNum.java
@@ -0,0 +1,16 @@
+package JavaCoding;
+
+public class AddTwoNum {
+    public static void main(String[] args){
+
+
+        int a=2;
+        int b= 3;
+
+        int sum=a+b;
+
+
+        System.out.println(sum);
+    }
+    
+}
