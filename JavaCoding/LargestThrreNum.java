4bd300c09cd898ec1252737197828fa1756e74a6
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-20 18:10:38 +0530

    largest number

---
+++ JavaCoding/LargestThrreNum.java
@@ -0,0 +1,30 @@
+package JavaCoding;
+
+public class LargestThrreNum {
+    public static void main(String[] args){
+
+        // Java Program to Find Largest of Three Numbers
+
+        int a=100;
+        int b=200;
+        int c=30;
+
+        if(a>b && a>b){
+
+            System.out.println(a + " a is largest number");
+        }
+        else if(b>a && b>c){
+            System.out.println(b + " b is largest number");
+
+        }
+        else{
+            System.out.println(c + " c is largest number");
+
+
+        }
+
+        
+
+    }
+    
+}
---
+++ JavaCoding/Sum.java
@@ -0,0 +1,20 @@
+package JavaCoding;
+
+public class Sum {
+    public static void main(String[] args){
+
+        //Sum of Numbers in Java
+
+        int n=10;
+
+        int sum=0;
+
+        for(int i=0; i<=n; i++){
+
+            sum=sum+i;
+        }
+        System.out.println(sum);
+
+    }
+    
+}
