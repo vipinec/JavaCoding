151fccf0225625453a0cc17580828673c0514fbd
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-20 16:43:42 +0530

    programs

---
+++ JavaCoding/AsciiValue.java
@@ -0,0 +1,17 @@
+package JavaCoding;
+
+public class AsciiValue {
+    public static void main(String[] args){
+
+
+        char ch ;
+
+        //int c=(ch);
+
+        for(int i=0; i<=100; i++){
+
+            System.out.println(" The ASCII value of " + (char)i + "  =  " + i);  
+        }
+    }
+    
+}
