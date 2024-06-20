2da9e046f2ba8fd666610990d06bff8a0eb66eb3
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-20 15:49:02 +0530

    reverse num

---
+++ JavaCoding/ReverseNumb.java
@@ -0,0 +1,22 @@
+package JavaCoding;
+
+public class ReverseNumb {
+
+    public static void main(String[] args){
+
+        int n=1234;
+
+        int rev=0;
+
+        while(n>0){
+
+            rev=(rev*10) + n%10;
+            n=n/10;
+
+        }
+        System.out.println(rev);
+
+
+    }
+    
+}
