55f947dc52341012d2720671f43169cbcd6865c4
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-20 15:19:06 +0530

    table of 5 is

---
+++ JavaCoding/Factorial.java
@@ -0,0 +1,20 @@
+package JavaCoding;
+
+public class Factorial {
+    public static void main(String[] args){
+
+
+        int n=5; 
+        int multi=1;
+
+        for (int i= 1; i<=10; i++){
+
+            multi= n*i;
+            System.out.print(multi + " ");
+
+
+
+        }
+    }
+    
+}
