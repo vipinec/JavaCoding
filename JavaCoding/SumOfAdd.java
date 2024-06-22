9d69a1b40d920293b038cab10bda2d9736a2550f
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-22 11:43:47 +0530

    add

---
+++ JavaCoding/SumOfAdd.java
@@ -0,0 +1,13 @@
+package JavaCoding;
+
+public class SumOfAdd {
+    public static void main(String[] args){
+
+        int a=10;
+        int b= 20;
+
+
+        System.out.println(a+b);
+    }
+    
+}
