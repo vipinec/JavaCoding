6575949bccdf5a3543e37b9dab53d4c2b19af3da
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-26 13:30:55 +0530

    pattern progrm

---
+++ JavaCoding/PatternVk.java
@@ -0,0 +1,19 @@
+package JavaCoding;
+
+public class PatternVk {
+    public static void main(String[] args){
+
+
+        for(int i=1; i<=5; i++){
+
+
+            for (int j=1; j<=5; j++){
+
+                System.out.print("1" );
+            }
+            System.out.print("\n");
+
+        }
+    }
+    
+}
