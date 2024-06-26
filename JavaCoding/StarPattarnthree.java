e703284afe087480d77587f94517a1094b820433
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-26 13:25:59 +0530

    hi

---
+++ JavaCoding/StarPattarnthree.java
@@ -0,0 +1,18 @@
+package JavaCoding;
+
+public class StarPattarnthree {
+    public static void main(String[] args){
+
+        for(int i=1; i<=5; i++){
+            for(int j=i; j<5; j++){
+            System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++)
+            System.out.print("*");
+
+        }
+        System.out.print("\n");
+    
+}
+    
+}
