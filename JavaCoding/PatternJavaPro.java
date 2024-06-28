6ec2475435e3f3bcd0e8342814c701c520cdd297
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-28 13:20:19 +0530

    add line radhe radhe

--- JavaCoding/PatternJavaPro.java
+++ JavaCoding/PatternJavaPro.java
@@ -11,7 +11,7 @@ public class PatternJavaPro {
                 System.out.print("*");
             }
 
-            System.out.print("\n");
+            System.out.print("\n" + " radhe radhe");
         }
 
 
