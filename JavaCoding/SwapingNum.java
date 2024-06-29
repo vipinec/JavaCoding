6b61b07f68acf8fa7efb876b344cdff406f5a5e3
mygitprojectw3learning <vipinec011@gmail.com>
2024-06-29 11:34:48 +0530

    swaping number

---
+++ JavaCoding/SwapingNum.java
@@ -0,0 +1,25 @@
+package JavaCoding;
+
+public class SwapingNum {
+    public static void main(String[] args){
+
+        int a=3;
+        int b=4;
+        int t;
+
+
+        System.out.println("Before swaping number is " +a+ " " +b);
+
+        t=a;
+        a=b;
+        b=t;
+
+        System.out.println("After swaping number is " +a+ " " +b);
+
+
+
+
+
+    }
+    
+}
