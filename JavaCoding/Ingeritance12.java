88be08c3c490193f152e3d96fcb4c82b3febaf7d
mygitprojectw3learning <vipinec011@gmail.com>
2024-07-08 01:33:23 +0530

    inheritance two classes

---
+++ JavaCoding/Ingeritance12.java
@@ -0,0 +1,20 @@
+package JavaCoding;
+
+public class Ingeritance12 extends Inheritance11 {
+
+    public static void main(String[] args){
+
+        Ingeritance12 obj = new Ingeritance12();
+
+         obj.apnabank();
+         
+        System.out.println(obj.name);
+        System.out.println(obj.salary);
+        System.out.println(obj.account);
+
+
+
+
+    }
+    
+}
---
+++ JavaCoding/Inheritance11.java
@@ -0,0 +1,16 @@
+package JavaCoding;
+
+public class Inheritance11 {
+
+    String name="aman";
+
+    int age = 24;
+    int salary=50000;
+    String account= "sbi bank";
+
+    public static void apnabank(){
+
+        System.out.println("testing bank account with inheritance");
+    }
+    
+}
